b75507865c7556e9ffb868bee7d9b021f715fae3
Jianlong Chen <jianlong99@gmail.com>
2014-05-12 17:50:12 +0800

    Add scripts

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ my-vim
 ======
 
 my vim configuration!
+
+
+### How to use
+
+clone this repo and type the follow command:
+
+    ./my_vim.sh
---
+++ my_vim.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# file my_vim.sh
+# author Jianlong Chen <jianlong99@gmail.com>
+# date 2014-02-21
+
+TOPDIR=`pwd`
+TEMPLATE_DIR=$TOPDIR/templates
+PLUGINS_DIR=$TOPDIR/plugins
+
+git submodule init
+git submodule update
+
+[ -f $HOME/.vimrc ] && cp $HOME/.vimrc $HOME/.vimrc_backup
+cp $TEMPLATE_DIR/vimrc-template $HOME/.vimrc
+mkdir -p $HOME/.vim/autoload
+mkdir -p $HOME/.vim/bundle
+cp $PLUGINS_DIR/vim-pathogen/autoload/pathogen.vim $HOME/.vim/autoload/pathogen.vim
+cp -r $PLUGINS_DIR/vim-template $HOME/.vim/bundle/
