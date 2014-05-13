6e51c4dc570affbdf17eed6937d504460e5ba7db
Jianlong Chen <jianlong99@gmail.com>
2014-05-13 21:25:43 +0800

    Add taglist plugin

--- my_vim.sh
+++ my_vim.sh
@@ -3,9 +3,15 @@
 # author Jianlong Chen <jianlong99@gmail.com>
 # date 2014-02-21
 
+# plugin list
+INSTALL_TEMPLATE_PLUGIN=1
+INSTALL_PROJECT_PLUGIN=1
+INSTALL_TAGLIST_PLUGIN=1
+
 TOPDIR=`pwd`
 TEMPLATE_DIR=$TOPDIR/templates
 PLUGINS_DIR=$TOPDIR/plugins
+
 
 git submodule init
 git submodule update
@@ -15,5 +21,19 @@ cp $TEMPLATE_DIR/vimrc-template $HOME/.vimrc
 mkdir -p $HOME/.vim/autoload
 mkdir -p $HOME/.vim/bundle
 cp $PLUGINS_DIR/vim-pathogen/autoload/pathogen.vim $HOME/.vim/autoload/pathogen.vim
-cp -r $PLUGINS_DIR/vim-template $HOME/.vim/bundle/
-cp -r $PLUGINS_DIR/vim-project $HOME/.vim/bundle/
+
+# vim-template plugin
+if [ $INSTALL_TEMPLATE_PLUGIN -ne 0 ]; then
+	cp -r --remove-destination $PLUGINS_DIR/vim-template $HOME/.vim/bundle/
+fi
+
+# vim-project plugin
+if [ $INSTALL_PROJECT_PLUGIN -ne 0 ]; then
+	cp -r --remove-destination $PLUGINS_DIR/vim-project $HOME/.vim/bundle/
+fi
+
+# vim-taglist plugin
+if [ $INSTALL_TAGLIST_PLUGIN -ne 0 ]; then
+	sudo apt-get install ctags
+	cp -r --remove-destination $PLUGINS_DIR/vim-taglist $HOME/.vim/bundle/
+fi
